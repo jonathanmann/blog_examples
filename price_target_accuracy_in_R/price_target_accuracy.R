f11e08db44efb285510d9177fc12097caedbcfce
Jonathan Mann <mann17@gmail.com>
2015-08-14 22:25:24 -0400

    update R code

--- price_target_accuracy_in_R/price_target_accuracy.R
+++ price_target_accuracy_in_R/price_target_accuracy.R
@@ -1,10 +1,10 @@
 require("ggplot2")
-dat2 <- read.csv(file="quarterly_sp_data.csv",head=TRUE,sep=",")
-ggplot(dat2, aes(x=dat2$Projected, y=dat2$Actual)) + geom_point(shape=1,size=.6,color="steelblue") + 
-  ylim(-.5, 1.5) + xlim(-.3,.7) + xlab("Projected") + ylab("Actual") + theme(
-    panel.background = element_rect(fill = "transparent",colour = NA), # or theme_blank()
+dat2<- read.csv(file="quarterly_stock_data.csv",head=TRUE,sep=",")
+ggplot(dat2, aes(x=dat2$ProjectedGrowth, y=dat2$ActualGrowth)) + geom_point(shape=1,size=.6,color="steelblue") + 
+  ylim(-1,2) + xlim(-1,2) +
+  xlab("Projected") + ylab("Actual") + theme(
+    panel.background = element_rect(fill = "transparent",colour = NA), 
     panel.grid.minor = element_blank(), 
     panel.grid.major = element_blank(),
     plot.background = element_rect(fill = "transparent",colour = NA)
   )
-
