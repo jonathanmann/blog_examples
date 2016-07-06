1017854d5484966a8fb79c5cefb88ad67c22aa9f
Jonathan Mann <mann17@gmail.com>
2016-07-06 00:53:44 -0400

    add d3 developer salary map

---
+++ Javascript/d3/js/script.js
@@ -0,0 +1,19 @@
+function tooltipHtml(n, d){	/* function to create html content string in tooltip div. */
+return "<h4>"+n+"</h4><table>"+
+    "<tr><td>Average</td><td>$"+(d.avg)+"K</td></tr>"+
+    "</table>";
+}
+
+var sampleData = {}
+var state_data = [];
+d3.csv("data/state_data.csv", function(data) {
+state_data = data;
+for (var e in state_data){
+var z = state_data[e]
+var i = z["id"]
+var s = z["Salary"] 
+sampleData[i]={id:i, avg:s, color:d3.interpolate("steelblue","black")((s -70)/100)}; 
+}
+//console.log(sampleData);
+uStates.draw("#statesvg", sampleData, tooltipHtml);
+});
