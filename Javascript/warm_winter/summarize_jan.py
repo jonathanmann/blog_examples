fbda985a31c9679b6a46c155be229f3d0475b1f8
Jonathan Mann <jonathan.mann@nyu.edu>
2021-01-19 18:47:22 -0500

    make Jan 19th updates to weather post

--- Javascript/warm_winter/summarize_jan.py
+++ Javascript/warm_winter/summarize_jan.py
@@ -3,10 +3,5 @@ import pandas as pd
 w = pd.read_csv('nyc_early_jan.csv')
 w['freezing'] = w.MeanTempF.apply(lambda x: 1 if x < 32.0 else 0)
 w['year'] = w.Date.apply(lambda x: x[:4])
-w = w[['year','freezing']]
-w.groupby('year').agg('sum').to_csv('freezing_year.csv')
-"""s = 4.21
-mean = 7.44
-Z Score = 1.77
-96.1636
-"""
+w = w[['year','freezing']] #.reindex(index=w.index[::-1])
+w.groupby('year').agg('sum').reset_index().iloc[::-1].to_json('freezing_year.json',orient='records')
